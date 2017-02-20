2109053fa8adb803c0c21a5ce58584c7ec973af5
Kirk Byers <ktbyers@twb-tech.com>
2017-02-20 13:02:32 -0800

    File copy improvements.

--- napalm_ios/file_copy.py
+++ napalm_ios/file_copy.py
@@ -46,9 +46,11 @@ class FileCopy(BaseFileCopy):
         raise NotImplementedError
 
     def _connect(self):
+        """Establish Secure Copy connection."""
         self._netmiko_scp_obj.establish_scp_conn()
 
     def _disconnect(self):
+        """Disconnect Secure Copy connection."""
         self._netmiko_scp_obj.close_scp_chan()
 
     def _verify_space_and_transfer(self):
@@ -56,7 +58,7 @@ class FileCopy(BaseFileCopy):
         if not self._verify_space_available():
             msg = "Insufficent space available on device."
             raise FileTransferException(msg)
-        self._execute_transfer(self)
+        self._execute_transfer()
 
     def _execute_transfer(self):
         """SCP transfer file."""
@@ -82,7 +84,7 @@ class FileCopy(BaseFileCopy):
                 return None
 
         # File transferred verify MD5
-        if self._remote_md5() != self._local_md5():
+        if not self._compare_md5():
             msg = "File transferred to remote device, but MD5 does not match."
             raise FileTransferException(msg)
 
@@ -91,7 +93,7 @@ class FileCopy(BaseFileCopy):
     def get_file(self):
         """
         Transfer file from control machine to remote network device.
-    
+
         Raises FileTransferException on failure.
         """
         return self._transfer_wrapper(direction='get')
@@ -99,24 +101,39 @@ class FileCopy(BaseFileCopy):
     def put_file(self):
         """
         Transfer file from control machine to remote network device.
-    
+
         Raises FileTransferException on failure.
         """
         return self._transfer_wrapper(direction='put')
 
     def _remote_md5(self):
-        return self._netmiko_scp_obj.remote_md5()
+        """Always return the MD5 from the remote device (maybe source or dest file)."""
+        if self.direction == 'put':
+            return self._netmiko_scp_obj.remote_md5(remote_file=self.dest_file)
+        elif self.direction == 'get':
+            return self._netmiko_scp_obj.source_md5
 
     def _local_md5(self):
-        return self._netmiko_scp_obj.file_md5()
+        """Always return the MD5 from the control machine (maybe source or dest file)."""
+        if self.direction == 'put':
+            return self._netmiko_scp_obj.source_md5
+        if self.direction == 'get':
+            return self._netmiko_scp_obj.file_md5(self.dest_file)
 
     def _compare_md5(self):
-        return self._netmiko_scp_obj.compare_md5()
+        """
+        MD5 comparison between source and dest file (after transfer).
+
+        Return boolean
+        """
+        return self._remote_md5() == self._local_md5()
 
     def _remote_space_available(self):
+        """Return space available on remote device."""
         return self._netmiko_scp_obj.remote_space_available()
 
     def _local_space_available(self):
+        """Return space available on control machine."""
         return self._netmiko_scp_obj.local_space_available()
 
     def _verify_space_available(self):
@@ -140,7 +157,9 @@ class FileCopy(BaseFileCopy):
         return self._netmiko_scp_obj.check_file_exists()
 
     def _remote_file_size(self):
+        """Return size of file on remote machine."""
         return self._netmiko_scp_obj.remote_file_size()
 
     def _local_file_size(self):
+        """Return size of file on control machine."""
         return self._netmiko_scp_obj.file_size
